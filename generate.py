efbb95616fdaa27936aec6cb0259cea43c824c58
atul <atul@tablecheck.com>
2022-04-13 20:03:58 +0900

    _

--- generate.py
+++ generate.py
@@ -19,6 +19,7 @@ from colorthief import ColorThief
 required_extensions = ["*.jpg", "*.jpeg", "*.png", "*.gif", "*.webp"]
 NUM_IMAGES_PER_PAGE = 100
 
+
 @dataclass(order=True)
 class UnderscoreImage:
     """ Image class """
@@ -54,6 +55,7 @@ def get_dominant_color(img):
     color_thief = ColorThief(img)
     return color_thief.get_color(quality=1)
 
+
 def generate_hash(filename: str) -> str:
     """Generate a hash of the file"""
     with open(filename, 'rb') as imagefile:
@@ -78,8 +80,29 @@ def meta_alt(image):
 
 def calculate_perceptural_hash(image):
     """ Calculate the perceptual hash of an image """
-    pil_image = resize_image(image)
+    pil_image = open_image(image)
     return imagehash.colorhash(pil_image, binbits=32)
+
+
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
+
+def crop_max_square(pil_img):
+    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
+
+
+def generate_thumbnail_if_not_exists(imagedata):
+    """ Generate a thumbnail for an image """
+    thumbfilepath = "./docs/thumbnails/" + imagedata.file_hash + ".jpg"
+    if not os.path.exists(thumbfilepath):
+        im = open_image(imagedata.path)
+        im_thumb = crop_max_square(im).resize((50, 50), Image.LANCZOS)
+        im_thumb.save(thumbfilepath, optimize=True, quality=20)
 
 
 def generaate_image_metadata(image_path) -> UnderscoreImage:
@@ -104,12 +127,13 @@ def dedupe(imagedata):
                 deduped_list.append(image)
     return deduped_list
 
+
 @timing
 def generate_dominant_colors(imagedata):
     return [get_dominant_color(imageset[0].path) for _, imageset in (enumerate(chunks(imagedata, NUM_IMAGES_PER_PAGE)))]
 
-def resize_image(img):
-    tdims = (500, 500)
+
+def open_image(img):
     im = Image.open(img)
     im = im.convert("RGB")
     return im
@@ -148,31 +172,64 @@ def process_all_images(images):
     return list(tqdm(pool_obj.imap(generaate_image_metadata, images), total=len(images)))
 
 
+@timing
+def generaet_all_thumbs(images):
+    Path("docs/thumbnails/").mkdir(parents=True, exist_ok=True)
+    pool_obj = multiprocessing.Pool()
+    return list(tqdm(pool_obj.imap(generate_thumbnail_if_not_exists, images), total=len(images)))
+
+
+def index_to_file(index):
+    filen = index // NUM_IMAGES_PER_PAGE
+    if filen == 0:
+        return "index.html"
+    return str(filen) + ".html"
+
+
+def remove_files_with_extension(ext):
+    for file in Path("./docs").glob(ext):
+        os.remove(file)
+
+
 if __name__ == "__main__":
     template = jinja2.Environment(
         loader=jinja2.FileSystemLoader('./')
     ).get_template('template.html')
 
     images = []
-    for paths in [Path('docs').rglob(ext) for ext in required_extensions]:
-        for path in paths:
+    for paths in [Path('docs/images').rglob(ext) for ext in required_extensions]:
+        for path in list(paths):
             images.append(path.relative_to('.'))
 
     print("Generating metadata...")
     imagedata = dedupe(process_all_images(images))
     shuffle(imagedata)
 
+    print("Generating thumbnails...")
+    generaet_all_thumbs(imagedata)
+
     imagedata = sort_greedy(imagedata)
 
     # dom_cols = generate_dominant_colors(imagedata)
 
     print("Rendering pages...")
+    remove_files_with_extension("*.html")
+    max_index = len(imagedata) // NUM_IMAGES_PER_PAGE
     for index, imageset in (enumerate(chunks(imagedata, NUM_IMAGES_PER_PAGE))):
         subs = template.render(title=index, imagedata=imageset,
-                               index=index, max_index=(len(imagedata) // NUM_IMAGES_PER_PAGE))
+                               index=index, max_index=max_index)
         output_file = "docs/{}.html".format(index)
         if index == 0:
             output_file = "docs/index.html"
         with open(output_file, 'w') as f:
             f.write(subs)
+
+    thumb = template.render(title="*", imagedata=imagedata,
+                            mode="thumb",
+                            index_to_file=index_to_file,
+                            enumerate=enumerate,
+                            index=max_index, max_index=max_index)
+    output_file = "docs/thumb.html"
+    with open(output_file, 'w') as f:
+        f.write(thumb)
     print("Done!")
